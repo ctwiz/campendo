51b5ed242405810303833c31476957cd510de3db
Dan Gailey <conrad.twizzle@gmail.com>
2013-05-30 17:44:23 -0700

    adding frameworks

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Campendo::Application.config.secret_token = 'ca7a6ac8f3e43196c66c5649261f6742e99fbd4200be7f2b6babd4ef270f5898cc6b3052b074346430971a00199a230e7363a8857fe6d148ebed47ba37734f51'
